b10dc1410d5359e5277e5c6af87f6b77a1150019
sasaco <sasaicco@gmail.com>
2021-03-25 18:42:13 +0900

    note から rss を取得しようとした

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -47,7 +48,8 @@ import { HomeContactComponent } from './home/contact/contact.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/home/news/news.component.ts
+++ src/app/home/news/news.component.ts
@@ -1,8 +1,44 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { parseString } from 'xml2js';
 
 @Component({
   selector: 'app-home-news',
   templateUrl: './news.component.html',
   styleUrls: ['../home.component.scss', './news.component.scss']
 })
-export class HomeNewsComponent { }
+export class HomeNewsComponent implements OnInit  {
+  constructor(private http: HttpClient) {}
+
+  public RssData: any[] = new Array();
+
+  ngOnInit(): void {
+
+    const requestOptions: Object = {
+      observe: "body",
+      responseType: "text"
+    };
+    this.http.get("https://note.com/structuralengine/rss", {
+      headers: new HttpHeaders({
+        'Access-Control-Allow-Origin': '*'
+      })
+    })
+    .subscribe(
+      response => {
+        parseString(response, (err, result) => {
+          if(err) {   
+              console.log('There was an error when parsing: ' + err);
+          } else {
+              console.log('The JSON version is: ' + JSON.stringify(result));
+              this.RssData.push({
+
+              })
+          }
+        })
+      },
+      error => {
+        console.log(error);
+      }
+    );
+  }
+ }
